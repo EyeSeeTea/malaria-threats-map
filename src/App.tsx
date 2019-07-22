e818b43b69ed4e77434aa79211437a8700d34ef0
David Mendez Gonzalez <david.gonzalez@getcruise.com>
2019-07-22 00:11:50 -0700

    initial mapbox gl package

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import * as mapboxgl from "mapbox-gl";
+import styled from "styled-components";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+Object.getOwnPropertyDescriptor(mapboxgl, "accessToken").set(
+  "pk.eyJ1IjoibW11a2ltIiwiYSI6ImNqNnduNHB2bDE3MHAycXRiOHR3aG0wMTYifQ.ConO2Bqm3yxPukZk6L9cjA"
+);
+
+const Map = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 100%;
+`;
+
+class App extends React.Component {
+  map: any;
+  mapContainer: any;
+
+  componentDidMount() {
+    this.map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: "mapbox://styles/mapbox/streets-v9"
+    });
+  }
+
+  componentWillUnmount() {
+    this.map.remove();
+  }
+
+  render() {
+    return <Map ref={el => (this.mapContainer = el)} />;
+  }
 }
 
 export default App;
