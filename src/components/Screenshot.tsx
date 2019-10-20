61477f0f9fd56653af07163d445741a4b0bb1860
David Mendez Gonzalez <davidmendezgonzalez@gmail.com>
2019-10-20 14:50:34 -0700

    fix download screen

--- src/components/Screenshot.tsx
+++ src/components/Screenshot.tsx
@@ -10,6 +10,18 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
+function dataURLtoBlob(dataurl: string) {
+  let arr = dataurl.split(","),
+    mime = arr[0].match(/:(.*?);/)[1],
+    bstr = atob(arr[1]),
+    n = bstr.length,
+    u8arr = new Uint8Array(n);
+  while (n--) {
+    u8arr[n] = bstr.charCodeAt(n);
+  }
+  return new Blob([u8arr], { type: mime });
+}
+
 function Screenshot({ map }: any) {
   const classes = useStyles({});
 
@@ -19,8 +31,9 @@ function Screenshot({ map }: any) {
       .toDataURL("image/png", 1.0)
       .replace("image/png", "image/octet-stream");
     const link = document.createElement("a");
+    const objurl = URL.createObjectURL(dataURLtoBlob(image));
     link.download = `MTM - ${new Date().toUTCString()}.png`;
-    link.href = image;
+    link.href = objurl;
     link.click();
   };
   return (
