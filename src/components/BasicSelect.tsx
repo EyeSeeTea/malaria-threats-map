25c039daf09cd7d77b5c4e9198ce7c0f587038e0
David Mendez Gonzalez <davidmendezgonzalez@gmail.com>
2019-12-03 22:22:09 -0800

    fixed multiselect input

--- src/components/BasicSelect.tsx
+++ src/components/BasicSelect.tsx
@@ -48,7 +48,6 @@ const useStyles = makeStyles((theme: Theme) =>
       }
     },
     valueContainer: {
-      display: "flex",
       flex: 1,
       alignItems: "center",
       overflow: "hidden"
