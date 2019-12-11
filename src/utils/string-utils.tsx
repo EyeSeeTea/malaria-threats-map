a19f2cf0658d24ce36ba0239b63d949e06945833
David Mendez Gonzalez <davidmendezgonzalez@gmail.com>
2019-12-10 21:39:19 -0800

    added basic translations for formatters

--- src/utils/string-utils.tsx
+++ src/utils/string-utils.tsx
@@ -1,27 +1,57 @@
 export const formatList = (list: string[]) => {
-  if (list.length === 0) {
-    return "";
-  } else if (list.length === 1) {
-    return list[0];
+  const lng = localStorage.getItem("language");
+  if (lng === "es") {
+    if (list.length === 0) {
+      return "";
+    } else if (list.length === 1) {
+      return list[0];
+    } else {
+      return (
+        list.slice(0, list.length - 1).join(", ") +
+        " y " +
+        list[list.length - 1]
+      );
+    }
   } else {
-    return (
-      list.slice(0, list.length - 1).join(", ") +
-      " and " +
-      list[list.length - 1]
-    );
+    if (list.length === 0) {
+      return "";
+    } else if (list.length === 1) {
+      return list[0];
+    } else {
+      return (
+        list.slice(0, list.length - 1).join(", ") +
+        " and " +
+        list[list.length - 1]
+      );
+    }
   }
 };
 
 export const formatYears = (from: string, to: string) => {
-  if (!from && !to) {
-    return "";
-  } else if (from && !to) {
-    return `in ${from}`;
-  } else if (!from && to) {
-    return `in ${to}`;
-  } else if (from === to) {
-    return `in ${to}`;
+  const lng = localStorage.getItem("language");
+  if (lng === "es") {
+    if (!from && !to) {
+      return "";
+    } else if (from && !to) {
+      return `en ${from}`;
+    } else if (!from && to) {
+      return `en ${to}`;
+    } else if (from === to) {
+      return `en ${to}`;
+    } else {
+      return `desde ${from} a ${to}`;
+    }
   } else {
-    return `between ${from} and ${to}`;
+    if (!from && !to) {
+      return "";
+    } else if (from && !to) {
+      return `in ${from}`;
+    } else if (!from && to) {
+      return `in ${to}`;
+    } else if (from === to) {
+      return `in ${to}`;
+    } else {
+      return `between ${from} and ${to}`;
+    }
   }
 };
