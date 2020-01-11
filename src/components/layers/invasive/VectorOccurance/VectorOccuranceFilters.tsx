7acb858c2731c39842670bfde651cf06a7911e01
David Mendez Gonzalez <davidmendezgonzalez@gmail.com>
2020-01-11 09:22:50 -0800

    added reporting form back on invasive theme

--- src/components/layers/invasive/VectorOccurance/VectorOccuranceFilters.tsx
+++ src/components/layers/invasive/VectorOccurance/VectorOccuranceFilters.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import YearRangeSelector from "../../../YearRangeSelector";
 import VectorSpeciesFilter from "../../../filters/VectorSpeciesFilter";
+import { Snackbar } from "../../../Filters";
+import { Link } from "@material-ui/core";
+import { useTranslation } from "react-i18next";
 
 export default function VectorOccuranceFilters() {
+  const { t } = useTranslation("common");
   return (
     <>
       <VectorSpeciesFilter />
       <YearRangeSelector />
+      <Snackbar>
+        {t("invasive.legend.vector_occurrance.link_content_1")}
+        <Link
+          href={t("prevention.legend.vector_ocurrence.link")}
+          target="_blank"
+        >
+          {t("invasive.legend.vector_occurrance.link_text")}
+        </Link>
+      </Snackbar>
     </>
   );
 }
