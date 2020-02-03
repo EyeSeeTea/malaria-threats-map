00fa25c221b5de0e3123521687d0b6a500f09f50
David Mendez Gonzalez <davidmendezgonzalez@gmail.com>
2020-02-02 16:53:36 -0800

    sort by name in countries after translations

--- src/components/filters/CountrySelector.tsx
+++ src/components/filters/CountrySelector.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import { setRegionAction } from "../../store/actions/base-actions";
 import {
@@ -14,6 +14,8 @@ import FormLabel from "@material-ui/core/FormLabel";
 import { Divider, FilterWrapper } from "./Filters";
 import T from "../../translations/T";
 import config from "../../config";
+import { useTranslation } from "react-i18next";
+import * as R from "ramda";
 
 const mapStateToProps = (state: State) => ({
   region: selectRegion(state),
@@ -30,39 +32,40 @@ type StateProps = ReturnType<typeof mapStateToProps>;
 type DispatchProps = typeof mapDispatchToProps;
 type Props = DispatchProps & StateProps;
 
-class CountrySelector extends Component<Props> {
-  onChange = (selection: any) => {
-    this.props.setRegion({ country: selection ? selection.value : undefined });
+const CountrySelector = ({
+  region,
+  countries = [],
+  mekongCountries,
+  setRegion
+}: Props) => {
+  const { t } = useTranslation("common");
+  const onChange = (selection: any) => {
+    setRegion({ country: selection ? selection.value : undefined });
   };
-  render() {
-    const { region, countries = [], mekongCountries } = this.props;
-    const suggestions: any[] = config.mekong
-      ? mekongCountries.map(country => ({
-          label: country.ISO_2_CODE,
-          value: country.ISO_2_CODE
-        }))
-      : countries.map((country: Translation) => ({
-          label: country.VALUE_,
-          value: country.VALUE_
-        }));
-    return (
-      <FilterWrapper>
-        <FormLabel component="legend">
-          <T i18nKey={"filters.country"} />
-        </FormLabel>
-        <Divider />
-        <IntegrationReactSelect
-          isClearable
-          placeholder={"Select Country"}
-          suggestions={suggestions}
-          onChange={this.onChange}
-          value={
-            suggestions.find((s: any) => s.value === region.country) || null
-          }
-        />
-      </FilterWrapper>
-    );
-  }
-}
+  const suggestions: any[] = config.mekong
+    ? mekongCountries.map(country => ({
+        label: t(country.ISO_2_CODE),
+        value: country.ISO_2_CODE
+      }))
+    : countries.map((country: Translation) => ({
+        label: t(country.VALUE_),
+        value: country.VALUE_
+      }));
+  return (
+    <FilterWrapper>
+      <FormLabel component="legend">
+        <T i18nKey={"filters.country"} />
+      </FormLabel>
+      <Divider />
+      <IntegrationReactSelect
+        isClearable
+        placeholder={"Select Country"}
+        suggestions={R.sortBy(R.prop("label"), suggestions)}
+        onChange={onChange}
+        value={suggestions.find((s: any) => s.value === region.country) || null}
+      />
+    </FilterWrapper>
+  );
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(CountrySelector);
