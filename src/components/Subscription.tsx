bc5f45a79da3515670db46e1cbe225753065b9f5
David Mendez Gonzalez <davidmendezgonzalez@gmail.com>
2020-06-21 20:35:49 -0700

    added translations to subscribe form

--- src/components/Subscription.tsx
+++ src/components/Subscription.tsx
@@ -12,7 +12,7 @@ import {
   MenuItem,
   Select,
   TextField,
-  Theme
+  Theme,
 } from "@material-ui/core";
 import SubscriptionIcon from "@material-ui/icons/RssFeed";
 import Typography from "@material-ui/core/Typography";
@@ -22,6 +22,7 @@ import { selectIsSubscriptionOpen } from "../store/reducers/base-reducer";
 import { connect } from "react-redux";
 import { addSubscriptionContactRequestAction } from "../store/actions/data-download-actions";
 import { Contact } from "./DataDownload";
+import { useTranslation } from "react-i18next";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -29,28 +30,28 @@ const useStyles = makeStyles((theme: Theme) =>
       marginTop: theme.spacing(1),
       "& > *": {
         margin: theme.spacing(1),
-        width: "25ch"
-      }
+        width: "25ch",
+      },
     },
     fab: {
       pointerEvents: "all",
-      margin: theme.spacing(0.5, 0.5)
+      margin: theme.spacing(0.5, 0.5),
     },
     paper: {
       margin: theme.spacing(1),
       padding: theme.spacing(3),
-      width: "100%"
-    }
+      width: "100%",
+    },
   })
 );
 
 const mapStateToProps = (state: State) => ({
-  subscriptionOpen: selectIsSubscriptionOpen(state)
+  subscriptionOpen: selectIsSubscriptionOpen(state),
 });
 
 const mapDispatchToProps = {
   setSubscriptionOpen: setSubscriptionOpenAction,
-  saveContact: addSubscriptionContactRequestAction
+  saveContact: addSubscriptionContactRequestAction,
 };
 type StateProps = ReturnType<typeof mapStateToProps>;
 type DispatchProps = typeof mapDispatchToProps;
@@ -59,8 +60,9 @@ type Props = DispatchProps & StateProps;
 const Subscription = ({
   subscriptionOpen,
   setSubscriptionOpen,
-  saveContact
+  saveContact,
 }: Props) => {
+  const { t } = useTranslation("common");
   const classes = useStyles({});
   const [email, setEmail] = React.useState<string>(null);
   const [firstName, setFirstName] = React.useState<string>(null);
@@ -82,7 +84,7 @@ const Subscription = ({
       firstName,
       lastName,
       organization,
-      country
+      country,
     };
     saveContact(contact);
   };
@@ -95,7 +97,7 @@ const Subscription = ({
         color={"default"}
         className={classes.fab}
         onClick={handleOpen}
-        title={"Subscription"}
+        title={t("subscription.button")}
       >
         <SubscriptionIcon />
       </Fab>
@@ -105,27 +107,27 @@ const Subscription = ({
         open={subscriptionOpen}
         onClose={handleClose}
         PaperProps={{
-          className: classes.paper
+          className: classes.paper,
         }}
       >
         <form id="ic_signupform">
           <div>
-            <Typography variant={"h5"}>
-              Subscribe to receive Threats Updates!
-            </Typography>
+            <Typography variant={"h5"}>{t("subscription.title")}</Typography>
           </div>
           <div>
             <FormControl fullWidth margin={"dense"}>
               <div data-validation-type="1" data-label="First Name">
                 <TextField
                   fullWidth
-                  label="First Name"
-                  type="text"
-                  placeholder="First Name"
+                  label={t("subscription.first_name")}
+                  type={t("subscription.first_name")}
+                  placeholder={t("subscription.first_name")}
                   name="data[fname]"
                   required
                   value={firstName}
-                  onChange={event => setFirstName(event.target.value as string)}
+                  onChange={(event) =>
+                    setFirstName(event.target.value as string)
+                  }
                 />
               </div>
             </FormControl>
@@ -133,13 +135,15 @@ const Subscription = ({
               <div data-validation-type="1" data-label="Last Name">
                 <TextField
                   fullWidth
-                  label="Last Name"
-                  type="text"
-                  placeholder="Last Name"
+                  label={t("subscription.last_name")}
+                  type={t("subscription.last_name")}
+                  placeholder={t("subscription.last_name")}
                   name="data[lname]"
                   required
                   value={lastName}
-                  onChange={event => setLastName(event.target.value as string)}
+                  onChange={(event) =>
+                    setLastName(event.target.value as string)
+                  }
                 />
               </div>
             </FormControl>
@@ -147,13 +151,13 @@ const Subscription = ({
               <div data-validation-type="1" data-label="Email">
                 <TextField
                   fullWidth
-                  label="Email"
-                  type="email"
-                  placeholder="Email"
+                  label={t("subscription.email")}
+                  type={t("subscription.email")}
+                  placeholder={t("subscription.email")}
                   required
                   name="data[email]"
                   value={email}
-                  onChange={event => setEmail(event.target.value as string)}
+                  onChange={(event) => setEmail(event.target.value as string)}
                 />
               </div>
             </FormControl>
@@ -165,13 +169,13 @@ const Subscription = ({
               >
                 <TextField
                   fullWidth
-                  label="Country"
-                  type="text"
-                  placeholder="Country"
+                  label={t("subscription.country")}
+                  type={t("subscription.country")}
+                  placeholder={t("subscription.country")}
                   required
                   name="data[country]"
                   value={country}
-                  onChange={event => setCountry(event.target.value as string)}
+                  onChange={(event) => setCountry(event.target.value as string)}
                 />
               </div>
             </FormControl>
@@ -181,12 +185,14 @@ const Subscription = ({
               data-label="Organization"
             >
               <FormControl fullWidth margin={"dense"}>
-                <InputLabel id="organization-label">Organization</InputLabel>
+                <InputLabel id="organization-label">
+                  {t("subscription.organization")}
+                </InputLabel>
                 <Select
                   name="data[organization]"
                   labelId="organization-label"
                   value={organization}
-                  onChange={event =>
+                  onChange={(event) =>
                     setOrganization(event.target.value as string)
                   }
                 >
@@ -212,7 +218,7 @@ const Subscription = ({
                 type="button"
                 onClick={() => submit()}
               >
-                Subscribe
+                {t("subscription.button")}
               </Button>
             </FormControl>
           </div>
