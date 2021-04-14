81decd9d0c63383c61ec024f069fb149fa1b7b71
Jorge Sánchez <xurxosf@gmail.com>
2021-04-14 12:32:03 +0200

    Add tests to open story mode for themes

---
+++ cypress/integration/invasive-vector-species/story-mode.spec.ts
@@ -0,0 +1,20 @@
+describe("Story mode", () => {
+    beforeEach(() => {
+        cy.visit("/");
+        cy.contains("Close").click();
+        cy.contains("INVASIVE VECTOR SPECIES").click();
+        cy.findByRole("progressbar").should('not.exist')
+        cy.findByTitle("Story mode").click();
+    })
+    it("should contains expected step 1 title in the story", () => {
+        cy.contains("The recent detection of An. stephensi in Sri Lanka and the horn of Africa underscores the potential for vector species to spread and establish in new geographical areas")
+    });
+    it("should contains expected step 2 title in the story", () => {
+        cy.findByText("2").click();
+        cy.contains("Timely detection of invasive vector species is crucial to contain their spread")
+    });
+    it("should contains expected step 3 title in the story", () => {
+        cy.findByText("3").click();
+        cy.contains("Control strategies should be informed by best practices from other countries, be adequately monitored and evaluated, and be modified where required")
+    });
+});
---
+++ cypress/integration/parasite-gene-deletions/story-mode.spec.ts
@@ -0,0 +1,20 @@
+describe("Story mode", () => {
+    beforeEach(() => {
+        cy.visit("/");
+        cy.contains("Close").click();
+        cy.contains("PARASITE pfhrp2/3 GENE DELETIONS").click();
+        cy.findByRole("progressbar").should('not.exist')
+        cy.findByTitle("Story mode").click();
+    })
+    it("should contains expected step 1 title in the story", () => {
+        cy.contains("Gene deletions among malaria parasites causes false-negative diagnostic test results")
+    });
+    it("should contains expected step 2 title in the story", () => {
+        cy.findByText("2").click();
+        cy.contains("pfhrp2/3 gene deletions may have significant implications for public health")
+    });
+    it("should contains expected step 3 title in the story", () => {
+        cy.findByText("3").click();
+        cy.contains("Increased monitoring of P. falciparum populations for pfhrp2/3 gene deletions is essential")
+    });
+});
