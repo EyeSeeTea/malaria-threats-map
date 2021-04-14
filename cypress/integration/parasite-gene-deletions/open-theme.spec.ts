b62772d8810d4ce0593dd40d69d71ffe338a6efd
Jorge Sánchez <xurxosf@gmail.com>
2021-04-14 12:31:06 +0200

    Add tests to open Parasite Drug Efficacy and Resistance theme

---
+++ cypress/integration/parasite-gene-deletions/open-theme.spec.ts
@@ -0,0 +1,14 @@
+describe("Open theme", () => {
+    beforeEach(() => {
+        cy.visit("/");
+        cy.contains("Close").click();
+        cy.contains("PARASITE pfhrp2/3 GENE DELETIONS").click();
+    })
+
+    it("should contains pfhrp2/3 gene deletions subtheme by default", () => {
+        cy.contains("pfhrp2/3 gene deletions");
+    });
+    it("should contains english language by default", () => {
+        cy.contains("English");
+    });
+});
