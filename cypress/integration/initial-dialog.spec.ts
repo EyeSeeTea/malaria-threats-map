ebbf0beec19e5af504b918b05ca313de4bc29dc2
Jorge Sánchez <xurxosf@gmail.com>
2021-04-14 12:29:31 +0200

    Modify default timeout and close tour guide

--- cypress/integration/initial-dialog.spec.ts
+++ cypress/integration/initial-dialog.spec.ts
@@ -1,17 +1,16 @@
-describe("Initial dialog", () => {
-    it("should contains the correct title", () => {
+describe("Open theme", () => {
+    beforeEach(() => {
         cy.visit("/");
+        cy.contains("Close").click();
+    })
 
+    it("should contains insecticide resistance status sub theme by default", () => {
         cy.contains("Malaria Threats Map");
     });
     it("should contains english language by default", () => {
-        cy.visit("/");
-
         cy.contains("English");
     });
     it("should contains 4 theme cards", () => {
-        cy.visit("/");
-
         cy.contains("VECTOR INSECTICIDE RESISTANCE");
         cy.contains("PARASITE pfhrp2/3 GENE DELETIONS");
         cy.contains("PARASITE DRUG EFFICACY AND RESISTANCE");
