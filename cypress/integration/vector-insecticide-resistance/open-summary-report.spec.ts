bde6e6c5b07c490845f5b55aee16ce2646eef373
Jorge Sánchez <xurxosf@gmail.com>
2021-04-15 09:53:26 +0200

    Add open summary report tests

---
+++ cypress/integration/vector-insecticide-resistance/open-summary-report.spec.ts
@@ -0,0 +1,12 @@
+import { themes } from "../../support/constants";
+
+describe("Story mode", () => {
+    beforeEach(() => {
+        cy.loadPage(themes.vectorInsecticideResistance);
+        cy.findByRole('button', { name: "Summary Report" }).click();
+    })
+
+    it("should open summary report dialog", () => {
+        cy.findByRole('heading', { name: "Overview of insecticide resistance in malaria vectors" })
+    })
+});
