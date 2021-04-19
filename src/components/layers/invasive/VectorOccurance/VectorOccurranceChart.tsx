086163ebb7fa7759b7202ba10e9aa0f4948904ab
Adrian Quintana <adrianquintana@gmail.com>
2021-04-19 13:22:58 +0100

    revert changes to yarn.lock and tsconfig

--- src/components/layers/invasive/VectorOccurance/VectorOccurranceChart.tsx
+++ src/components/layers/invasive/VectorOccurance/VectorOccurranceChart.tsx
@@ -9,7 +9,7 @@ import * as R from "ramda";
 import { InvasiveStudy } from "../../../../types/Invasive";
 import Citation from "../../../charts/Citation";
 import { getMonthFromNumber } from "./utils";
-import { lowerCase } from "lower-case";
+import { lowerCase } from "change-case";
 import Pagination from "../../../charts/Pagination";
 import { useTranslation } from "react-i18next";
 import { ChartContainer } from "../../../Chart";
