2d619ae672300cd207857345b49d7202c6321909
Jorge Sánchez <xurxosf@gmail.com>
2021-04-21 12:26:33 +0200

    Inject CompositionRoot in epics

--- src/webapp/store/index.tsx
+++ src/webapp/store/index.tsx
@@ -1,19 +1,26 @@
-import { createEpicMiddleware } from "redux-observable";
-import { applyMiddleware, compose, createStore } from "redux";
+import {createEpicMiddleware} from "redux-observable";
+import {applyMiddleware, compose, createStore} from "redux";
 
 import rootEpic from "./epics";
 import createRootReducer from "./reducers";
+import {CompositionRoot} from "../../CompositionRoot";
 
-const epicMiddleware = createEpicMiddleware();
+export type EpicDependencies = {
+    compositionRoot: CompositionRoot;
+};
+
+const dependencies = {
+    compositionRoot: new CompositionRoot(),
+};
+
+const epicMiddleware = createEpicMiddleware({dependencies});
+
 const middleware = [epicMiddleware];
 
 export default function configureStore() {
-  const composeEnhancer: typeof compose =
-    (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-  const store = createStore(
-    createRootReducer(),
-    composeEnhancer(applyMiddleware(...middleware))
-  );
-  epicMiddleware.run(rootEpic as any);
-  return { store };
+    const composeEnhancer: typeof compose =
+        (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+    const store = createStore(createRootReducer(), composeEnhancer(applyMiddleware(...middleware)));
+    epicMiddleware.run(rootEpic as any);
+    return {store};
 }
