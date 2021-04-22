fb55dffaa6ba427eddb1353d85ae932eead9a75c
Jocelyn Dunkley <jocelyn.dunkley09@gmail.com>
2021-04-22 10:52:04 +0200

    invasive steps 1 and 2

---
+++ src/components/story/invasive/InvasiveStep1.tsx
@@ -0,0 +1,76 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import { createStyles, Link, makeStyles } from "@material-ui/core";
+import { useTranslation, Trans } from "react-i18next";
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    title: {
+      lineHeight: 1.3,
+    },
+  })
+);
+
+export default () => {
+  const classes = useStyles({});
+  const { t } = useTranslation("invasiveTranslation");
+  return (
+    <div>
+      <Typography variant={"h6"} className={classes.title}>
+      <Trans i18nKey="step1.part1" t={t}>
+        <b>
+          The recent detection of <i>An. stephensi</i> in Sri Lanka and the horn
+          of Africa underscores the potential for vector species to spread and
+          establish in new geographical areas.
+        </b>
+        </Trans>
+ 
+      </Typography>
+      <br />
+      <Typography variant={"body2"}>
+      <Trans i18nKey="step1.part2" t={t}>
+        <b>
+          <i>An. Gambiae</i> ’s invasion of northern Brazil in 1930 provides
+          another example. Its elimination between 1930s and early 40s proves
+          that vector invasions can be controlled.
+        </b>
+    </Trans>
+      </Typography>
+      <br />
+      <Typography variant={"body2"}>
+      <Trans i18nKey="step1.part3" t={t}>
+        <i>An. stephensi</i> is a highly efficient urban malaria vector
+        traditionally reported from certain countries in South-East Asia and the
+        Arabian Peninsula. Since 2012, it has been detected in Djibouti (2012),
+        Ethiopia (2016), Sri Lanka (2017) and most recently in the Republic of
+        the Sudan (2019, unpublished report).
+        </Trans>
+      </Typography>
+      <br />
+      <Typography variant={"body2"}>
+      <Trans i18nKey="step1.part4" t={t}>
+        The invasion of <i>An. stephensi</i> has contributed to the resurgence
+        of malaria in Djibouti City. In Sri Lanka, the recent invasion of this
+        vector could jeopardize efforts to prevent the re-establishment of
+        malaria. In Africa, given the rapid and uncontrolled growth of cities,
+        further spread and establishment of <i>An. stephensi</i> in urban
+        environments could put at risk the gains in malaria burden reduction
+        made since 2000.
+        </Trans>
+      </Typography>
+      <br />
+      <Typography variant={"body2"}>
+        <Link
+          href="https://www.who.int/publications/i/item/vector-alert-anopheles-stephensi-invasion-and-spread"
+          target="_blank"
+          color={"textSecondary"}
+        >
+          <img
+            src="https://apps.who.int/iris/rest/bitstreams/1283866/retrieve"
+            alt={t("supporting")}
+          />
+        </Link>
+      </Typography>
+    </div>
+  );
+};
---
+++ src/components/story/invasive/InvasiveStep2.tsx
@@ -0,0 +1,91 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import { createStyles, Link, makeStyles } from "@material-ui/core";
+import { useTranslation, Trans } from "react-i18next";
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    title: {
+      lineHeight: 1.3
+    }
+  })
+);
+
+export default () => {
+  const classes = useStyles({});
+  const { t } = useTranslation("invasiveTranslation");
+  return (
+    <div>
+      <Typography variant={"h6"} className={classes.title}>
+      <Trans i18nKey="step2.part1" t={t}>
+        <b>
+          Timely detection of invasive vector species is crucial to contain
+          their spread.
+        </b>
+        </Trans>
+      </Typography>
+      <br />
+      <Typography variant={"body2"}>
+      <Trans i18nKey="step2.part2" t={t}>
+        <b>
+          Entomological surveillance systems provide critical information to
+          inform the design and implementation of vector control strategies.
+        </b>
+        </Trans>
+      </Typography>
+      <br />
+      <Typography variant={"body2"}>
+      <Trans i18nKey="step2.part3" t={t}>
+        Understanding vector species’ breeding and resting habitats, behaviour,
+        feeding preferences and resistance status is required to design
+        effective interventions to prevent further spread of invasive vector
+        species and establishment in new geographical areas.
+        </Trans>
+      </Typography>
+      <br />
+      <Typography variant={"body2"}>
+      <Trans i18nKey="step2.part4" t={t}>
+        WHO recommends that all malaria endemic countries conduct entomological
+        surveillance and report detection of suspected invasive species to WHO
+        by filling and sending the
+        </Trans>
+        <Link
+          target="_blank"
+          href="https://www.who.int/publications/i/item/who-form-to-report-detection-of-invasive-anopheles-vector-species"
+          color={"textSecondary"}
+        >
+          {t("WHO form to report detection of invasive Anopheles vector species")}
+        </Link>
+        to{" "}
+        <Link href="mailto:vectorsurveillance@who.int" color={"textSecondary"}>
+          vectorsurveillance@who.int
+        </Link>
+        .
+      </Typography>
+      <br />
+      <Typography variant={"body2"}>
+        {t("Key indicators for entomological surveillance entomological surveillance are listed in chapter 5 of the")}
+        <Link
+          target="_blank"
+          href="https://www.who.int/malaria/publications/atoz/9789241565578/en/"
+          color={"textSecondary"}
+        >
+         {t("WHO Malaria Surveillance, monitoring and evaluation: a reference manual.")} 
+        </Link>
+      </Typography>
+      <br />
+      <Typography variant={"body2"}>
+        <Link
+          href="https://www.who.int/publications/i/item/9789241565578"
+          target="_blank"
+          color={"textSecondary"}
+        >
+          <img
+            src="https://www.who.int/malaria/publications/atoz/978921565578-eng.jpg"
+            alt="supporting"
+          />
+        </Link>
+      </Typography>
+    </div>
+  );
+};
