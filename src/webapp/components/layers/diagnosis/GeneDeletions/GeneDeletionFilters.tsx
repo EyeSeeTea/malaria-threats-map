ddf37405d21382b27c01ed3826a20d176d879b27
Jocelyn Dunkley <jocelyn.dunkley09@gmail.com>
2021-05-11 13:23:41 +0200

    use material design snackbar instead of custom component

--- src/webapp/components/layers/diagnosis/GeneDeletions/GeneDeletionFilters.tsx
+++ src/webapp/components/layers/diagnosis/GeneDeletions/GeneDeletionFilters.tsx
@@ -1,18 +1,26 @@
 import React from "react";
+import { useTranslation, Trans } from "react-i18next";
 import YearRangeSelector from "../../../YearRangeSelector";
 import SurveyTypeFilter from "../../../filters/SurveyTypeFilter";
 import PatientTypeFilter from "../../../filters/PatientTypeFilter";
 import DeletionTypeFilter from "../../../filters/DeletionTypeFilter";
-import Pfhrp23Note from "./Pfhrp23Note";
+import { Snackbar } from "../../../Filters";
 
 export default function GeneDeletionFilters() {
+    const { t } = useTranslation("common");
+
     return (
         <>
             <DeletionTypeFilter />
             <SurveyTypeFilter />
             <PatientTypeFilter />
             <YearRangeSelector minYear={1998} maxYear={new Date().getFullYear()} />
-            <Pfhrp23Note />
+            <Snackbar>
+            <Trans i18nKey="filters.pfhrp23_note" t={t}>
+                    Across surveys, the criteria for selecting samples to test for<i>pfhrp 2/3</i>deletions varies;
+                    therefore, refer to the full report cited for more details.
+                </Trans>
+            </Snackbar>
         </>
     );
 }
