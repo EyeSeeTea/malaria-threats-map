312062d2f0f9466e79a2ee6e9578d029a0c78347
Jocelyn Dunkley <jocelyn.dunkley09@gmail.com>
2021-07-27 16:26:16 +0200

    remove some commented code

--- src/webapp/components/layers/PBOEndemicityLayer.tsx
+++ src/webapp/components/layers/PBOEndemicityLayer.tsx
@@ -31,7 +31,6 @@ type Props = {
 
 class PBOEndemicityLayer extends Component<Props> {
     componentDidMount(): void {
-        console.log("here!!");
         const source: any = {
             type: "geojson",
             data: `${config.mapServerUrl}/1/query?where=ENDEMICITY%3D0&f=geojson&geometryPrecision=2.5`,
