c7b53d85c2a2ce3d21a53687d64babe8882523ae
Jorge Sánchez <xurxosf@gmail.com>
2021-11-16 11:05:35 +0100

    Use countryLayer response in endemicity layer

--- src/webapp/components/layers/EndemicityLayer.tsx
+++ src/webapp/components/layers/EndemicityLayer.tsx
@@ -2,7 +2,9 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { State } from "../../store/types";
 import { selectEndemicity } from "../../store/reducers/base-reducer";
-import config from "../../config";
+import { selectCountryLayer } from "../../store/reducers/country-layer-reducer";
+import { CountryLayer } from "../../../domain/entities/CountryLayer";
+import { fetchCountryLayerRequest } from "../../store/actions/country-layer-actions";
 
 const ENDEMICITY_LAYER_ID = "endemicity-layer";
 const ENDEMICITY_SOURCE_ID = "endemicity-source";
@@ -21,30 +23,31 @@ const layer: mapboxgl.FillLayer = {
 };
 
 const mapStateToProps = (state: State) => ({
+    countryLayer: selectCountryLayer(state),
     endemicity: selectEndemicity(state),
 });
 
-class EndemicityLayer extends Component<any> {
-    componentDidMount(): void {
-        const source: any = {
-            type: "geojson",
-            data: `${config.mapServerUrl}/1/query?where=ENDEMICITY%3D0&f=geojson&geometryPrecision=2.5`,
-        };
-        this.props.map.addSource(ENDEMICITY_SOURCE_ID, source);
-        this.props.map.addLayer(layer);
-        if (this.props.endemicity) {
-            this.showLayer();
-        } else {
-            this.hideLayer();
-        }
+const mapDispatchToProps = {
+    fetchCountryLayer: fetchCountryLayerRequest,
+};
+
+type StateProps = ReturnType<typeof mapStateToProps>;
+type DispatchProps = typeof mapDispatchToProps;
+
+type OwnProps = {
+    map: mapboxgl.Map;
+};
+type Props = StateProps & DispatchProps & OwnProps;
+
+class EndemicityLayer extends Component<Props> {
+    componentDidMount() {
+        this.fetchCountryLayerIfRequired();
+        this.mountLayer();
     }
 
     componentDidUpdate() {
-        if (this.props.endemicity) {
-            this.showLayer();
-        } else {
-            this.hideLayer();
-        }
+        this.fetchCountryLayerIfRequired();
+        this.mountLayer();
     }
 
     componentWillUnmount(): void {
@@ -63,9 +66,54 @@ class EndemicityLayer extends Component<any> {
         }
     };
 
+    private mountLayer() {
+        const existing = this.props.map.getSource(ENDEMICITY_SOURCE_ID);
+        if (!existing && this.props.countryLayer) {
+            const countryLayer: CountryLayer = this.props.countryLayer;
+
+            const noEndemicCountryLayer = mapCountryLayer({
+                ...countryLayer,
+                features: countryLayer.features.filter(feature => feature.properties.ENDEMICITY === 0),
+            });
+
+            const source: any = {
+                type: "geojson",
+                data: noEndemicCountryLayer,
+            };
+
+            this.props.map.addSource(ENDEMICITY_SOURCE_ID, source);
+            this.props.map.addLayer(layer);
+        }
+
+        if (this.props.endemicity) {
+            this.showLayer();
+        } else {
+            this.hideLayer();
+        }
+    }
+
+    fetchCountryLayerIfRequired() {
+        if (!this.props.countryLayer && this.props.endemicity) {
+            this.props.fetchCountryLayer();
+        }
+    }
+
     render() {
         return <div />;
     }
 }
+export default connect(mapStateToProps, mapDispatchToProps)(EndemicityLayer);
 
-export default connect(mapStateToProps, null)(EndemicityLayer);
+function mapCountryLayer(countryLayer: CountryLayer): GeoJSON.FeatureCollection<GeoJSON.Geometry> {
+    return {
+        type: "FeatureCollection",
+        features: countryLayer.features.map(feature => {
+            return {
+                type: "Feature",
+                id: feature.id,
+                geometry: feature.geometry,
+                properties: feature.properties,
+            };
+        }),
+    };
+}
