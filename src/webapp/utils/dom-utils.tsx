ba0bbf411902a4a3091213fe90efde5ba4ebc319
Jorge Sánchez <xurxosf@gmail.com>
2022-02-23 12:56:03 +0100

    Revert comment

--- src/webapp/utils/dom-utils.tsx
+++ src/webapp/utils/dom-utils.tsx
@@ -1,9 +1,9 @@
-export const dispatchCustomEvent = (_event: string) => {
-    // if (navigator.userAgent.indexOf("MSIE") !== -1 || navigator.appVersion.indexOf("Trident/") > 0) {
-    //     const evt = document.createEvent("UIEvents");
-    //     evt.initEvent(event, true, false);
-    //     window.dispatchEvent(evt);
-    // } else {
-    //     window.dispatchEvent(new Event(event));
-    // }
+export const dispatchCustomEvent = (event: string) => {
+    if (navigator.userAgent.indexOf("MSIE") !== -1 || navigator.appVersion.indexOf("Trident/") > 0) {
+        const evt = document.createEvent("UIEvents");
+        evt.initEvent(event, true, false);
+        window.dispatchEvent(evt);
+    } else {
+        window.dispatchEvent(new Event(event));
+    }
 };
