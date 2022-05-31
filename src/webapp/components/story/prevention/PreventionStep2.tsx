f092e7fbeecd99509555e915986d9708684167d9
Jordi <jordi.burguet.castell@gmail.com>
2022-05-31 14:56:33 +0200

    Add react-markdown and read the translations of story mode from the xMART database.

--- src/webapp/components/story/prevention/PreventionStep2.tsx
+++ src/webapp/components/story/prevention/PreventionStep2.tsx
@@ -1,53 +1,15 @@
 import React from "react";
-import Typography from "@mui/material/Typography";
-import { Link } from "@mui/material";
-import createStyles from "@mui/styles/createStyles";
-import makeStyles from "@mui/styles/makeStyles";
-import { useTranslation, Trans } from "react-i18next";
-
-const useStyles = makeStyles(() =>
-    createStyles({
-        title: {
-            lineHeight: 1.3,
-        },
-    })
-);
+import { useTranslation } from "react-i18next";
+import ReactMarkdown from "react-markdown";
 
 const PreventionStep2 = () => {
-    const classes = useStyles({});
     const { t } = useTranslation();
 
     return (
         <div>
-            <Typography variant={"h6"} className={classes.title}>
-                <Trans i18nKey="preventionStory.step2.part1" t={t}>
-                    <b>Monitoring has found that insecticide resistance is widespread in malaria vectors</b>
-                </Trans>
-            </Typography>
-            <br />
-            <Typography variant={"body2"}>
-                <Trans i18nKey="preventionStory.step2.part2" t={t}>
-                    <b>
-                        65 of 89 malaria-endemic countries have reported pyrethroid resistance in at least one local
-                        vector since 2010
-                    </b>
-                </Trans>
-            </Typography>
-            <br />
-            <Typography variant={"body2"}>
-                <Trans i18nKey="preventionStory.step2.part3" t={t}>
-                    Resistance has been found in vectors from all six WHO regions and to all four classes of insecticide
-                    currently used in adult malaria vector control. Pyrethroid resistance is most commonly tested and
-                    reported.
-                </Trans>
-            </Typography>
-            <br />
-            <Link href={t("preventionStory.step2.link")} target="_blank" color={"textSecondary"}>
-                <img
-                    src="https://www.who.int/malaria/publications/atoz/9789241514057-eng.jpg"
-                    alt={t("preventionStory.step2.prevention2")}
-                />
-            </Link>
+            <ReactMarkdown>
+                {t("preventionStory_step2")}
+            </ReactMarkdown>
         </div>
     );
 };
--- src/webapp/store/epics/translations-epics.tsx
+++ src/webapp/store/epics/translations-epics.tsx
@@ -18,7 +18,7 @@ export const getTranslationsEpic = (action$: Observable<ActionType<typeof fetchT
         switchMap(() => {
             return ajax
                 .getUrl<TranslationXMartResponse>(
-                    "https://frontdoor-r5quteqglawbs.azurefd.net/TRAINING_EYESEETEA/MTM_TRADS?$top=9999"
+                    "https://frontdoor-r5quteqglawbs.azurefd.net/TRAINING_EYESEETEA/MTM_TRADS?$top=99996"
                 )
                 .pipe(
                     mergeMap((response: TranslationXMartResponse) => {
