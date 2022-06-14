b5697dc636a832281c8be4b989f7f73f2cf59ef0
xurxodev <xurxosf@gmail.com>
2022-06-14 06:53:50 +0200

    Bring https://github.com/EyeSeeTea/malaria-threats-map/pull/139

--- src/webapp/store/actions.tsx
+++ src/webapp/store/actions.tsx
@@ -24,7 +24,6 @@ export enum ActionTypeEnum {
     SetDataDownloadOpen = "malaria/SET_DATA_DOWNLOAD_OPEN",
     SetReportOpen = "malaria/SET_REPORT_OPEN",
     SetMapTitle = "malaria/SET_MAP_TITLE",
-    SetSubscriptionOpen = "malaria/SET_SUBSCRIPTION_OPEN",
     SetUploadFileOpen = "malaria/SET_UPLOAD_FILE_OPEN",
     SetFeedbackOpen = "malaria/SET_FEEDBACK_OPEN",
     SetTheaterMode = "malaria/SET_THEATER_MODE_OPEN",
@@ -97,10 +96,6 @@ export enum ActionTypeEnum {
     FetchDownloadsError = "downloads/FETCH_DOWNLOADS_ERROR",
 
     AddDownloadRequest = "downloads/ADD_DOWNLOAD_REQUEST",
-
-    AddSubscriptionContactRequest = "downloads/ADD_SUBSCRIPTION_CONTACT_REQUEST",
-    AddSubscriptionContactSuccess = "downloads/ADD_SUBSCRIPTION_CONTACT_SUCCESS",
-    AddSubscriptionContactError = "downloads/ADD_SUBSCRIPTION_CONTACT_ERROR",
 
     UploadFileRequest = "uploadFile/REQUEST",
     UploadFileSuccess = "uploadFile/SUCCESS",
