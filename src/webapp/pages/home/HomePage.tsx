aad018705713bf4bf741e43d5b9f9173e41ba206
xurxodev <xurxosf@gmail.com>
2022-07-05 10:05:07 +0200

    Refactor themes section

--- src/webapp/pages/home/HomePage.tsx
+++ src/webapp/pages/home/HomePage.tsx
@@ -3,12 +3,14 @@ import React from "react";
 import Layout from "../layout/Layout";
 import BannerSection from "./BannerSection";
 import LinkCardsSection from "./LinkCardsSection";
+import ThemesSection from "./ThemesSection";
 
 const HomePage: React.FC = () => {
     return (
         <Layout>
             <BannerSection />
             <LinkCardsSection />
+            <ThemesSection />
         </Layout>
     );
 };
