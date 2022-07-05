aedfb8d81fa36ce04072df7df9ffaa7b6ee0d346
xurxodev <xurxosf@gmail.com>
2022-07-05 10:04:45 +0200

    Refactor link cards section

--- src/webapp/pages/home/HomePage.tsx
+++ src/webapp/pages/home/HomePage.tsx
@@ -2,11 +2,13 @@ import React from "react";
 
 import Layout from "../layout/Layout";
 import BannerSection from "./BannerSection";
+import LinkCardsSection from "./LinkCardsSection";
 
 const HomePage: React.FC = () => {
     return (
         <Layout>
             <BannerSection />
+            <LinkCardsSection />
         </Layout>
     );
 };
