e9444c2a7b66c65627738c1417a6f9f130624c46
Jordi BC <jordi.burguet.castell@eyeseetea.com>
2022-08-04 19:59:05 +0200

    Read invasive species from xmart.
    This is a temporary solution while waiting for having
    and access to the latest data through a view in xmart.

--- src/data/repositories/InvasiveApiRepository.ts
+++ src/data/repositories/InvasiveApiRepository.ts
@@ -1,6 +1,6 @@
 import { request } from "../common/request";
 import { FutureData } from "../../domain/common/FutureData";
-import { ApiParams, ApiResponse } from "../common/types";
+import { XMartApiResponse } from "../common/types";
 import { InvasiveRepository } from "../../domain/repositories/InvasiveRepository";
 import { InvasiveStudy } from "../../domain/entities/InvasiveStudy";
 
@@ -8,14 +8,8 @@ export class InvasiveApiRepository implements InvasiveRepository {
     constructor(private baseUrl: string) {}
 
     getStudies(): FutureData<InvasiveStudy[]> {
-        const params: ApiParams = {
-            f: "json",
-            where: `1=1`,
-            outFields: "*",
-        };
+        const url = "https://frontdoor-r5quteqglawbs.azurefd.net/TRAINING_EYESEETEA/MTM_INVASIVE_SPECIES";
 
-        return request<ApiResponse<InvasiveStudy>>({ url: `${this.baseUrl}/10/query`, params }).map(response =>
-            response.features.map(feature => feature.attributes)
-        );
+        return request<XMartApiResponse<InvasiveStudy>>({ url }).map(response => response.value);
     }
 }
