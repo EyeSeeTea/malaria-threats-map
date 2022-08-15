ed0da28d0172d7c9068f837fd8da62b4bdaf6717
xurxodev <xurxosf@gmail.com>
2022-08-15 09:08:59 +0200

    format the code

--- src/webapp/components/layers/studies-filters.tsx
+++ src/webapp/components/layers/studies-filters.tsx
@@ -19,12 +19,14 @@ export const DELETION_TYPES = {
     },
 };
 
-export const filterByYearRange = (years: number[], allowEmpty = false) => (study: any) => {
-    return (
-        (allowEmpty && !study.YEAR_START) ||
-        (parseInt(study.YEAR_START) >= years[0] && parseInt(study.YEAR_START) <= years[1])
-    );
-};
+export const filterByYearRange =
+    (years: number[], allowEmpty = false) =>
+    (study: any) => {
+        return (
+            (allowEmpty && !study.YEAR_START) ||
+            (parseInt(study.YEAR_START) >= years[0] && parseInt(study.YEAR_START) <= years[1])
+        );
+    };
 
 export const filterByYears = (years: number[]) => (study: any) => {
     return !years.length || years.includes(study.YEAR_START);
