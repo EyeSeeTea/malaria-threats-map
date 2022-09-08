d9f89e27f1d4261a85ba4b77d6590ac6c9b38c99
Jordi BC <jordi.burguet.castell@eyeseetea.com>
2022-09-08 13:52:47 -0400

    Create steps in a straightforward way in PreventionSteps.tsx
    Addresses https://github.com/EyeSeeTea/malaria-threats-map/pull/215#discussion_r965515511

--- src/webapp/components/story/prevention/PreventionSteps.tsx
+++ src/webapp/components/story/prevention/PreventionSteps.tsx
@@ -1,7 +1,26 @@
-import PreventionStep1 from "./PreventionStep1";
-import PreventionStep2 from "./PreventionStep2";
-import PreventionStep3 from "./PreventionStep3";
-import PreventionStep4 from "./PreventionStep4";
+import React from "react";
+import { useTranslation } from "react-i18next";
+import ReactMarkdown from "react-markdown";
 
-const PreventionSteps = [PreventionStep1, PreventionStep2, PreventionStep3, PreventionStep4];
+const createStep = (key: string) => {
+    const PreventionStep = () => {
+        const { t } = useTranslation();
+
+        return (
+            <div>
+                <ReactMarkdown>{t(key)}</ReactMarkdown>
+            </div>
+        );
+    };
+
+    return PreventionStep;
+};
+
+const PreventionSteps = [
+    "preventionStory_step1",
+    "preventionStory_step2",
+    "preventionStory_step3",
+    "preventionStory_step4",
+].map(createStep);
+
 export default PreventionSteps;
