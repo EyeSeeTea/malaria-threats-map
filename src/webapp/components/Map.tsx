8fa8b8e63b52dbe7549e90296300e79f4ffcffe2
David Gomes <davidbgomes92@gmail.com>
2022-10-12 11:11:54 +0100

    Change footer layout, add disclaimer & refactor links

--- src/webapp/components/Map.tsx
+++ src/webapp/components/Map.tsx
@@ -44,7 +44,6 @@ import MekongLayer from "./layers/MekongLayer";
 import Screenshot from "./Screenshot";
 import Report from "./Report";
 import Feedback from "./Feedback";
-import InitialDisclaimer from "./InitialDisclaimer";
 import TheaterMode from "./TheaterMode";
 import TourIcon from "./TourIcon";
 import ShareIcon from "./ShareIcon";
@@ -405,7 +404,6 @@ class Map extends React.Component<Props, StateTypes> {
                     <Fade in={false}>
                         <TopRightContainer>
                             <StoryModeSelector />
-                            <InitialDisclaimer />
 
                             <Feedback />
                             <TourIcon />
@@ -418,7 +416,6 @@ class Map extends React.Component<Props, StateTypes> {
                     <Fade in={showOptions}>
                         <TopRightVerticalContainer>
                             <StoryModeSelector />
-                            <InitialDisclaimer />
 
                             <Feedback />
                         </TopRightVerticalContainer>
