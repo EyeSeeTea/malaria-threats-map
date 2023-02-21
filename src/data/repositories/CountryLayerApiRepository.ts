2d297eab7870c66736beaa213d63185fffec7731
xurxodev <xurxosf@gmail.com>
2023-02-21 08:22:17 +0100

    Retrieve country data from new tables

--- src/data/repositories/CountryLayerApiRepository.ts
+++ src/data/repositories/CountryLayerApiRepository.ts
@@ -1,9 +1,9 @@
 import { request } from "../common/request";
 import { FutureData } from "../../domain/common/FutureData";
-import { ApiParams, CountryData, XMartApiResponse } from "../common/types";
+import { ApiParams } from "../common/types";
 import { CountryLayer } from "../../domain/entities/CountryLayer";
-import { Future } from "../../common/Future";
 import { CountryLayerRepository } from "../../domain/repositories/CountryLayerRepository";
+import { getBackendCountries } from "./common/getBackendCountries";
 
 export class CountryLayerApiRepository implements CountryLayerRepository {
     constructor(private baseUrl: string, private xmartBaseUrl: string) {}
@@ -15,7 +15,7 @@ export class CountryLayerApiRepository implements CountryLayerRepository {
             outFields: "OBJECTID,ADM0_SOVRN,ADM0_NAME,CENTER_LAT,CENTER_LON,ISO_2_CODE,ENDDATE",
         };
 
-        return this.getBackendCountries().flatMap(backendCountries => {
+        return getBackendCountries(this.xmartBaseUrl).flatMap(backendCountries => {
             return request<CountryLayer>({
                 url: `${this.baseUrl}/Detailed_Boundary_ADM0/FeatureServer/0/query`,
                 params,
@@ -48,14 +48,5 @@ export class CountryLayerApiRepository implements CountryLayerRepository {
                 return newCountryLayer;
             });
         });
-    }
-
-    private getBackendCountries(): FutureData<CountryData[]> {
-        return request<XMartApiResponse<CountryData>>({ url: `${this.xmartBaseUrl}/FACT_ENDEMICITY_REGIONS` })
-            .map(response => response.value)
-            .flatMapError(error => {
-                console.log("error loading countries from xmart", error);
-                return Future.success([]);
-            });
     }
 }
