fe473cdf6cb9828d6dd9b847121eca8816c98afc
xurxodev <xurxosf@gmail.com>
2023-03-10 08:17:51 +0100

    Remove 120 line without label

--- src/webapp/pages/dashboards/prevention/phenotypic-insecticide-resistance/MosquitoMortalityOverTimeDashboard.tsx
+++ src/webapp/pages/dashboards/prevention/phenotypic-insecticide-resistance/MosquitoMortalityOverTimeDashboard.tsx
@@ -159,6 +159,8 @@ function chartOptions(years: number[], data: MosquitoOverTimeData, xAxisVisible:
         chart: {
             type: "boxplot",
             height: 300,
+            marginTop: 30,
+            marginBottom: 30,
         },
 
         title: {
@@ -188,19 +190,8 @@ function chartOptions(years: number[], data: MosquitoOverTimeData, xAxisVisible:
             title: {
                 text: "",
             },
-            labels: {
-                formatter: function () {
-                    const label = this.axis.defaultLabelFormatter.call(this);
-
-                    // Use thousands separator for four-digit numbers too
-                    if (+label > 100) {
-                        return "";
-                    }
-                    return label;
-                },
-            },
             min: 0,
-            max: 120,
+            max: 100,
         },
 
         series: [
