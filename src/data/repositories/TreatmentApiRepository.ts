b0593b10980c2ceeb854be830045dc3c49be41ea
xurxodev <xurxosf@gmail.com>
2023-06-26 12:13:02 +0200

    Retrieve mock data for AMDER TES from TreatmentRepository

--- src/data/repositories/TreatmentApiRepository.ts
+++ src/data/repositories/TreatmentApiRepository.ts
@@ -3,6 +3,8 @@ import { FutureData } from "../../domain/common/FutureData";
 import { ApiParams, ApiResponse } from "../common/types";
 import { TreatmentRepository } from "../../domain/repositories/TreatmentRepository";
 import { TreatmentStudy } from "../../domain/entities/TreatmentStudy";
+import factAmdeoTes from "./data/FACT_AMDERO_TES.json";
+import { Future } from "../../common/Future";
 
 export class TreatmentApiRepository implements TreatmentRepository {
     constructor(private baseUrl: string) {}
@@ -14,8 +16,11 @@ export class TreatmentApiRepository implements TreatmentRepository {
             outFields: "*",
         };
 
-        return request<ApiResponse<TreatmentStudy>>({ url: `${this.baseUrl}/6/query`, params }).map(response =>
-            response.features.map(feature => feature.attributes)
-        );
+        return request<ApiResponse<TreatmentStudy>>({ url: `${this.baseUrl}/6/query`, params })
+            .map(response => response.features.map(feature => feature.attributes))
+            .flatMap(studies => {
+                const factAmdeoTesStudies = factAmdeoTes as unknown as TreatmentStudy[];
+                return Future.success([...studies, ...factAmdeoTesStudies]);
+            });
     }
 }
