aa9d4d38466e232f3956564eda1c205fa15c8571
mariaozamiz <ozamiz.m@gmail.com>
2023-10-06 19:54:37 +0200

    style: add box-shadow for contrast over the white map and delimit the box

--- src/webapp/components/MapInfoSummaryLegend.tsx
+++ src/webapp/components/MapInfoSummaryLegend.tsx
@@ -108,6 +108,8 @@ const MapInfoSummaryLegendContainer = styled.div`
     border-radius: 12px;
     background-color: #ffffff;
     width: 100%;
+    box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14),
+        0px 1px 3px 0px rgba(0, 0, 0, 0.12);
 `;
 
 const InfoRowContainer = styled.div`
