95b1df0d684c4951cabda3d405351a1f33d21897
mariaozamiz <ozamiz.m@gmail.com>
2023-10-06 20:13:43 +0200

    style: add a box-shadow to match the other legend box

--- src/webapp/components/MapScreenshot.tsx
+++ src/webapp/components/MapScreenshot.tsx
@@ -210,6 +210,8 @@ const LegendContainer = styled.div`
     .MuiButtonBase-root {
         padding-bottom: 0;
     }
+    box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14),
+        0px 1px 3px 0px rgba(0, 0, 0, 0.12);
 `;
 
 const StyledImage = styled.img<{ $hasSidebar?: boolean }>`
