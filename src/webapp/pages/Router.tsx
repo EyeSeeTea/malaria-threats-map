6bfc48070c8db0cdc3e036c6851814871acafd82
mariaozamiz <ozamiz.m@gmail.com>
2023-10-23 11:21:34 +0200

    chore: retrieve DisplaySuggestionModal from Router to aim concrete pages

--- src/webapp/pages/Router.tsx
+++ src/webapp/pages/Router.tsx
@@ -8,7 +8,6 @@ import { StoriesPage } from "./stories/StoriesPage";
 import HomePage from "./home/HomePage";
 import MapPage from "./map/MapPage";
 import ShareDataPage from "./shareData/ShareDataPage";
-import DisplaySuggestionModal from "../components/DisplaySuggestionModal";
 
 const ScrollToTop = (): JSX.Element => {
     const { pathname } = useLocation();
@@ -24,13 +23,11 @@ export const Router: React.FC = React.memo(() => {
     return (
         <HashRouter>
             <ScrollToTop />
-            <DisplaySuggestionModal />
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/about" element={<AboutPage />} />
                 <Route path="/contact" element={<ContactPage />} />
                 <Route path="/share-data" element={<ShareDataPage />} />
-
                 <Route path="/maps" element={<MapPage />} />
                 <Route path="/download" element={<DownloadPage />} />
                 <Route path="/stories" element={<StoriesPage />} />
