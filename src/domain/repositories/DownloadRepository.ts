8327edec59df3f9e4ee8f404b8e0c2dd515c1d89
xurxodev <xurxosf@gmail.com>
2024-01-26 07:00:47 +0100

    Format the code

--- src/domain/repositories/DownloadRepository.ts
+++ src/domain/repositories/DownloadRepository.ts
@@ -1,6 +1,6 @@
 import { FutureData } from "../common/FutureData";
 import { Download } from "../entities/Download";
 
-export interface  DownloadRepository {
-    save(download:Download): FutureData<void> 
+export interface DownloadRepository {
+    save(download: Download): FutureData<void>;
 }
