253b7db8ceae686a620ae4fe07cd1c076536da04
Chukwudumebi Onwuli <37223065+deeonwuli@users.noreply.github.com>
2024-08-21 13:28:37 +0100

    feat: add missing t&c link

--- src/webapp/components/DataDownload/steps/Terms.tsx
+++ src/webapp/components/DataDownload/steps/Terms.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Box, Button, Card, Checkbox, FormControlLabel, Grid, List, Stack, Typography } from "@mui/material";
-import { useTranslation } from "react-i18next";
+import { Trans, useTranslation } from "react-i18next";
 import { connect } from "react-redux";
 import styled from "styled-components";
 import { DatabaseSelection, TermsInfo } from "../types";
@@ -19,6 +19,8 @@ const Terms = ({ onChange, termsInfo, selectedDatabases, onChooseOther }: OwnPro
         onChange("agreement", event.target.checked);
     };
 
+    const termsAndConditionsLink = "https://www.who.int/about/policies/publishing/data-policy/terms-and-conditions";
+
     return (
         <React.Fragment>
             <Grid container rowSpacing={3} columnSpacing={4}>
@@ -28,7 +30,19 @@ const Terms = ({ onChange, termsInfo, selectedDatabases, onChooseOther }: OwnPro
                     </Typography>
                     <TermsContainer>
                         <div>
-                            <Typography variant={"body1"}>{t("common.data_download.terms_step.p1")}</Typography>
+                            <Typography variant={"body1"}>
+                                <Trans i18nKey="common.data_download.terms_step.p1" t={t}>
+                                    The use of all data is subject to the{" "}
+                                    <a href={termsAndConditionsLink}>Terms and conditions</a>
+                                    of use for WHO compilations, aggregations, evaluations and analyses. By using the
+                                    Datasets or any presentations of data derived from them, you agree to be bound by
+                                    and adhere to these Terms and Conditions, as may be amended from time to time by WHO
+                                    at its sole discretion. If any datasets are credited to a source other than WHO,
+                                    then those materials are not covered by these Terms and Conditions, and permission
+                                    should be sought from the source.
+                                </Trans>
+                            </Typography>
+
                             <br />
                             <Typography variant={"h5"} gutterBottom>
                                 {t("common.data_download.terms_step.acknowledgements")}
@@ -46,7 +60,13 @@ const Terms = ({ onChange, termsInfo, selectedDatabases, onChooseOther }: OwnPro
                                 />
                             }
                             label={
-                                <Typography variant={"body2"}>{t("common.data_download.terms_step.check")}</Typography>
+                                <Typography variant={"body2"}>
+                                    <Trans i18nKey="common.data_download.terms_step.check" t={t}>
+                                        Check here to indicated that you have read and agree to the{" "}
+                                        <a href={termsAndConditionsLink}>Terms and Conditions</a>
+                                        of use for WHO data compilations, aggregations, evaluations and analyses.
+                                    </Trans>
+                                </Typography>
                             }
                         />
                     </TermsContainer>
